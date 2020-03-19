fbf060c8a7fdb8d00da028c45484e5fe1d5456b0
sturkiewicz <62306641+sturkiewicz@users.noreply.github.com>
2020-03-19 14:29:29 +0100

    Exercise done and corrected

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,7 +7,7 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
-print("Pi = ", pi , " , and it is of a type ", type(pi))
+print("Pi = {} , and it is of a type {}".format(pi,type(pi)))
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
@@ -27,7 +27,8 @@ elif picked_fruit=='banana':
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
 def multiply(a, b):
-    print(a*b)
+    value = a * b
+    return value
 
 # TODO: Now call the function a few times to calculate the following answers
 
@@ -36,9 +37,8 @@ def multiply(a, b):
 
 
 
-print("12 x 96 =",)
-multiply(12, 96)
-print("48 x 17 =",)
-multiply(48, 17)
-print("196523 x 87323 =",)
-multiply(196523, 87323)
+print("12 x 96 =", multiply(12, 96))
+
+print("48 x 17 =", multiply(48, 17))
+
+print("196523 x 87323 =", multiply(196523, 87323))
