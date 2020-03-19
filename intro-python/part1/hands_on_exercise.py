a2ce418ea86b6bebdf213404f848af4e9c9b2197
sturkiewicz <62306641+sturkiewicz@users.noreply.github.com>
2020-03-19 14:14:20 +0100

    Update hands_on_exercise.py

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,24 +7,38 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
-
+print("Pi = ", pi , " , and it is of a type ", type(pi))
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
 
+if (i<<50 or i>>50):
+    print(i)
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
-
+if picked_fruit=='orange':
+    print('Kolor pomaranczy to pomaranczowy')
+elif picked_fruit=='strawberry':
+    print('Kolor truskawki to czerwony')
+elif picked_fruit=='banana':
+    print('Kolor banana to żółty')
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
-
+def multiply(a, b):
+    print(a*b)
 
 # TODO: Now call the function a few times to calculate the following answers
 
+
+
+
+
+
 print("12 x 96 =",)
-
+multiply(12, 96)
 print("48 x 17 =",)
-
+multiply(48, 17)
 print("196523 x 87323 =",)
+multiply(196523, 87323)
